97a816e9e26f9c2ed5fc5b00c043069a67ed66ab
Ciocirlan Robert <ciocirlan.r@mail.com>
2020-06-13 20:55:15 +0300

    Made input field required

--- src/server/index.js
+++ src/server/index.js
@@ -39,5 +39,5 @@ app.post('/test', async (req, res, next) => {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+    console.log('App listening on port 8081!')
 });
